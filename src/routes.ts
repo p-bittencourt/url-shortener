97ca412eeca5652b4094311053d9ca0db7a2acf8
pedro <pedromonteirobittencourt@gmail.com>
2025-01-24 15:07:35 -0300

    feat: implemented dns lookup to check for valid url and getDomain function

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,13 @@
 import { Application, Request, Response } from 'express';
+import dns from 'node:dns';
 
 interface shortUrlInterface {
   original_url: string;
   short_url: number;
+}
+
+interface errorOutput {
+  error: string;
 }
 
 const urls: shortUrlInterface[] = [];
@@ -16,11 +21,41 @@ export const createRoutes = (app: Application) => {
 
   app.post('/api/shorturl', function (req: Request, res: Response) {
     const url = req.body.url;
-    const content: shortUrlInterface = {
-      original_url: url,
-      short_url: urls.length + 1,
-    };
-    urls.push(content);
-    res.json(content);
+    const domain = getDomain(url);
+
+    if (!domain) {
+      res.json({ error: 'invalid url' });
+      return;
+    }
+
+    let content: shortUrlInterface | errorOutput;
+    dns.lookup(domain, (err) => {
+      if (err) {
+        content = { error: 'invalid url' };
+        res.json(content);
+      } else {
+        content = {
+          original_url: url,
+          short_url: urls.length + 1,
+        };
+        urls.push(content);
+        res.json(content);
+      }
+    });
   });
 };
+
+function getDomain(urlString: string) {
+  try {
+    const url = new URL(urlString);
+    let hostname = url.hostname;
+
+    if (hostname.startsWith('www.')) {
+      hostname = hostname.slice(4);
+    }
+
+    return hostname;
+  } catch {
+    return null;
+  }
+}
